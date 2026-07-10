0098e9468b9cf1bc8e21110177f10194a89edfeb
Junaid Gul <junaidgul4646@gmail.com>
2026-07-11 03:18:24 +0500

    Add code for DOM basics and element selection

---
+++ 04-DOM/01-DOM-Basics/script.js
@@ -0,0 +1,15 @@
+console.log("JavaScript is working!")
+// console.log(document)
+// console.log(document.URL)
+// console.log(document.title)
+// console.log(document.head)
+// console.log(document.body.children)
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+console.log(document.body.children);
+console.log(document.body.childNodes)
+
+
---
+++ 04-DOM/02-Selecting-Elements/script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title")
+title.textContent = "Fronted Master"
+console.log(title)
+let para = document.getElementById("para")
+console.log(para)
+
+const heading = document.getElementsByClassName("heading")
+console.log(heading)
+heading[1].textContent = "Change the Dom"
+console.log(heading[1])
+
